6b4e56ff870a55cc611f0e61a2561d05bf5ae4ad
abdyraman <abdyramanova.j@gmail.com>
2023-06-01 21:42:38 -0400

    arc

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,6 +1,4 @@
-
 var level = 10;
-
 // Trig to calc meter point
 function gaugePointer(wfreq) {
   var degrees = 180 - wfreq * 20;
@@ -39,7 +37,7 @@ function buildgauge(wfreq) {
       textinfo: 'text',
       textposition: 'inside',
       marker: {
-        colors: [
+      colors: [
           'rgba(14, 127, 0, .5)','rgba(14, 127, 0, .5)','rgba(14, 127, 0, .5)',
 'rgba(110, 154, 22, .5)','rgba(170, 202, 42, .5)' ,'rgba(202, 209, 95, .5)' ,'rgba(210, 206, 145, .5)' ,'rgba(232, 226, 202, .5)' ,'rgba(255, 255, 255, 0)' 
         ]
@@ -79,6 +77,100 @@ function buildgauge(wfreq) {
   let GAUGE = document.getElementById("gauge");
   Plotly.newPlot(GAUGE, data, layout);
 }
+
+// // Trig to calc meter point
+// function gaugePointer(wfreq) {
+//   var level = wfreq*20;
+//   var degrees = 180 - level;
+//   var radius = 0.5;
+//   var radians = degrees * Math.PI / 180;
+//   var x = radius * Math.cos(radians);
+//   var y = radius * Math.sin(radians);
+
+//   // Path: may have to change to create a better triangle
+//   var mainPath = 'M -.0 -0.05 L .0 0.05 L ';
+//   var pathX = String(x);
+//   var space = ' ';
+//   var pathY = String(y);
+//   var pathEnd = ' Z';
+//   var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+//   return path;
+// }
+
+// function buildgauge(wfreq) {
+//   var level = wfreq*20;
+//   var degrees = 180 - level;
+//   var radius = 0.5;
+//   var radians = degrees * Math.PI / 180;
+//   var x = radius * Math.cos(radians);
+//   var y = radius * Math.sin(radians);
+
+//   // Path: may have to change to create a better triangle
+//   var mainPath = 'M -.0 -0.05 L .0 0.05 L ';
+//   var pathX = String(x);
+//   var space = ' ';
+//   var pathY = String(y);
+//   var pathEnd = ' Z';
+//   var path = mainPath.concat(pathX, space, pathY, pathEnd);
+//   var data = [
+//     {
+//       type: 'scatter',
+//       x: [0],
+//       y: [0],
+//       marker: { size: 18, color: '850000' },
+//       showlegend: false,
+//       name: 'Belly Button',
+//       text: level,
+//       hoverinfo: 'text+name'
+//     },
+//     {
+//       values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9,50/9,50/9,50/9, 50],
+//       rotation: 70,
+//       text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+//       textinfo: 'text',
+//       textposition: 'inside',
+//       marker: {
+//         colors: [
+//           'rgba(14, 127, 0, .5)','rgba(14, 127, 0, .5)','rgba(14, 127, 0, .5)',
+// 'rgba(110, 154, 22, .5)','rgba(170, 202, 42, .5)' ,'rgba(202, 209, 95, .5)' ,'rgba(210, 206, 145, .5)' ,'rgba(232, 226, 202, .5)' ,'rgba(255, 255, 255, 0)' 
+//         ]
+//       },
+//       labels: ['9', '8', '7', '6', '5', '4', '3','2','1',''],
+//       hoverinfo: 'label',
+//       hole: 0.5,
+//       type: 'pie',
+//       showlegend: false
+//     }
+//   ];
+
+//   var layout = {
+//     shapes: [{
+//       type: 'path',
+//       path: path,
+//       fillcolor: '850000',
+//       line: {
+//         color: '850000'
+//       }
+//     }],
+//     autosize: true,
+//     xaxis: {
+//       zeroline: false,
+//       showticklabels: false,
+//       showgrid: false,
+//       range: [-1, 1]
+//     },
+//     yaxis: {
+//       zeroline: false,
+//       showticklabels: false,
+//       showgrid: false,
+//       range: [-1, 1]
+//     }
+//   };
+
+//   let GAUGE = document.getElementById("gauge");
+//   Plotly.newPlot(GAUGE, data, layout);
+// }
 
 
 // DRAFT
