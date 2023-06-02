a0304ec1d6ab1f62aed0157d83a56a6568ac52c7
abdyraman <abdyramanova.j@gmail.com>
2023-06-01 20:12:54 -0400

    Update bonus.js

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,77 +1,4 @@
 
-// function buildgauge(wfreq) {
-//   let data = [
-//     {
-//       domain: { x: [0, 1], y: [0, 1] },
-//       title: {
-//         text: "Belly Button Washing Frequency<br><span style='font-size: 16px'>Scrubs per week</span>",
-//         font: { size: 24 }
-//       },
-//       type: "indicator",
-//       mode: "gauge",
-//       value: wfreq,
-//       gauge: {
-//         shape: "angular",
-//         bar: {
-//           color: "blue",
-//           line: {
-//             color: "red",
-//             width: 4
-//           },
-//           thickness: 0
-//         },
-//         axis: {
-//           range: [0, 9],
-//           tickwidth: 1,
-//           tickcolor: "darkblue",
-//           tickvals: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-//           ticktext: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"]
-//         },
-//         bgcolor: "white",
-//         steps: [
-//           { range: [0, 1], color: "#F5F5DC" },
-//           { range: [1, 2], color: "#E8F5E9" },
-//           { range: [2, 3], color: "#C8E6C9" },
-//           { range: [3, 4], color: "#A5D6A7" },
-//           { range: [4, 5], color: "#81C784" },
-//           { range: [5, 6], color: "#66BB6A" },
-//           { range: [6, 7], color: "#4CAF50" },
-//           { range: [7, 8], color: "#43A047" },
-//           { range: [8, 9], color: "#388E3C" }
-//         ]
-//       }
-//     }
-//   ];
-
-//   var theta = 180;
-//   var r = 0.7;
-
-
-//   let layout = {
-//     xaxis: { range: [0, 1], showgrid: false, zeroline: false, visible: false },
-//     yaxis: { range: [0, 1], showgrid: false, zeroline: false, visible: false },
-//     showlegend: false,
-//     annotations: [
-//       {
-//         ax: 0.5,
-//         ay: 0,
-//         axref: "x",
-//         ayref: "y",
-//         x: 0.5 + r * Math.cos(Math.PI / 180 * theta),
-//         y: wfreq,
-//         xref: "x",
-//         yref: "y",
-//         showarrow: true,
-//         arrowhead: 2,
-//       }
-//     ]
-//   };
-
-//   let GAUGE = document.getElementById("gauge");
-//   Plotly.newPlot(GAUGE, data, layout);
-// }
-
-// Enter a speed between 0 and 180
 var level = 10;
 
 // Trig to calc meter point
@@ -153,3 +80,78 @@ function buildgauge(wfreq) {
   Plotly.newPlot(GAUGE, data, layout);
 }
 
+
+// DRAFT
+// function buildgauge(wfreq) {
+//   let data = [
+//     {
+//       domain: { x: [0, 1], y: [0, 1] },
+//       title: {
+//         text: "Belly Button Washing Frequency<br><span style='font-size: 16px'>Scrubs per week</span>",
+//         font: { size: 24 }
+//       },
+//       type: "indicator",
+//       mode: "gauge",
+//       value: wfreq,
+//       gauge: {
+//         shape: "angular",
+//         bar: {
+//           color: "blue",
+//           line: {
+//             color: "red",
+//             width: 4
+//           },
+//           thickness: 0
+//         },
+//         axis: {
+//           range: [0, 9],
+//           tickwidth: 1,
+//           tickcolor: "darkblue",
+//           tickvals: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+//           ticktext: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"]
+//         },
+//         bgcolor: "white",
+//         steps: [
+//           { range: [0, 1], color: "#F5F5DC" },
+//           { range: [1, 2], color: "#E8F5E9" },
+//           { range: [2, 3], color: "#C8E6C9" },
+//           { range: [3, 4], color: "#A5D6A7" },
+//           { range: [4, 5], color: "#81C784" },
+//           { range: [5, 6], color: "#66BB6A" },
+//           { range: [6, 7], color: "#4CAF50" },
+//           { range: [7, 8], color: "#43A047" },
+//           { range: [8, 9], color: "#388E3C" }
+//         ]
+//       }
+//     }
+//   ];
+
+//   var theta = 180;
+//   var r = 0.7;
+
+
+//   let layout = {
+//     xaxis: { range: [0, 1], showgrid: false, zeroline: false, visible: false },
+//     yaxis: { range: [0, 1], showgrid: false, zeroline: false, visible: false },
+//     showlegend: false,
+//     annotations: [
+//       {
+//         ax: 0.5,
+//         ay: 0,
+//         axref: "x",
+//         ayref: "y",
+//         x: 0.5 + r * Math.cos(Math.PI / 180 * theta),
+//         y: wfreq,
+//         xref: "x",
+//         yref: "y",
+//         showarrow: true,
+//         arrowhead: 2,
+//       }
+//     ]
+//   };
+
+//   let GAUGE = document.getElementById("gauge");
+//   Plotly.newPlot(GAUGE, data, layout);
+// }
+
+// Enter a speed between 0 and 180
